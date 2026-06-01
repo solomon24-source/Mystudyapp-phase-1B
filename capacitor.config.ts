f69ca18b6ec22df46bbf88d302c21e5fbf85df92
solomon24-source <solomonenyinnaya25@icloud.com>
2026-06-01 15:04:41 +0100

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,7 @@
+import { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'com.mystudyapp.phase1b',
+  appName: 'MyStudyApp',
+  webDir: 'www'
+};
+export default config;
